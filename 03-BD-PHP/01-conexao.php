15561431e45ebc2ac842adb65655ffd234e84e83
FelipeTerra <38055797+FelipeTerra@users.noreply.github.com>
2021-07-13 14:16:13 -0400

    Create 01-conexao.php
    Criando a conexão com o banco no MYSQL

---
+++ 03-BD-PHP/01-conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+$host    = "127.0.0.1";
+$user    = "root";
+$passwr  = "";
+$db      = "vendas";
+
+$mysqli = new mysqli($host, $user, $passwr, $db);
+$conecado = false;
+
+if (!$mysqli->connect_errno){
+    echo "<p>Conectado ao banco de dados!</p>";
+    $conecado = true;
+}
+else
+echo "<p>Não conectado ao banco de dados. Erro na conexão</p>";
+
+?>
