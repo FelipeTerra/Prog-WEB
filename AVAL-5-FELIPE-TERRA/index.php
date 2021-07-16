8f13b1b2956ed40973c2861edb779fc1e2b93809
FelipeTerra <38055797+FelipeTerra@users.noreply.github.com>
2021-07-15 21:49:31 -0400

    2

--- AVAL-5-FELIPE-TERRA/index.php
+++ AVAL-5-FELIPE-TERRA/index.php
@@ -15,13 +15,13 @@ require_once("conexao.php");
 
     <link rel="stylesheet" href="./Styles.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat: wght @ 100; 200; 300; 400; 500; 600; 700; 800; 900 & display = swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat: wght @ 100; 200; 300; 400; 500; 600; 700; 800; 900 & display = swap" rel="stylesheet">
 </head>
 
 <body>
-    <header><h1>Cadastro de Produtos</h1></header>
+    <header>
+        <h1>Cadastro de Produtos</h1>
+    </header>
 
     <main class="container">
         <section id="form">
@@ -30,53 +30,22 @@ require_once("conexao.php");
             <form action="conteudo.php" method="POST">
                 <imput type="hidden" name="id" value="">
 
-                <table id="table-form">
+                    <table id="table-form">
+                        <thead>
+                            <tr>
+                                <th><input class="card" id="id_nome" type="text" name="nome" placeholder="Nome do Pruduto" /></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><button id="button" type="submit" name="acao" value="Incluir">
+                                        <h1>Cadastrar</h1>
+                                    </button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+            </form>
 
-                    <?php
-                        
-
-                        // echo "<p><input class=\"card\" type=\"text\" name=\"nome\" placeholder=\"Nome do Produto\"></p>";
-                        if($valida_erro['nome']['ok']){
-                            echo "<tr>";
-                                echo "<td id=\"table-name\">Nome</td>";
-                                echo "<td><input class=\"card-normal\" type=\"text\" name=\"nome\" value= ".$valida_erro['nome']['valor']."></td>";
-                            echo "<tr>";                    
-                        }
-                        else {
-                            echo "<tr>";
-                                echo "<td id=\"table-name\">Nome</td>";
-                                echo "<td><input class=\"card-normal\" type=\"text\" name=\"nome\" value= ".$valida_erro['nome']['valor']."></td>";
-                            echo "<tr>";
-                        }
-                    
-                        if($valida_erro['marca']['ok']){
-                            echo "<tr>";
-                                echo "<td id=\"table-name\">Marca</td>";
-                                echo "<td><input class=\"card-normal\" type=\"text\" name=\"marca\" value= ".$valida_erro['marca']['valor']."></td>";
-                            echo "<tr>";  
-                        }
-                    
-                        if($valida_erro['quantidade']['ok']){
-                            echo "<tr>";
-                                echo "<td id=\"table-name\">Quantidade</td>";
-                                echo "<td><input class=\"card-normal\" type=\"number\" name=\"quantidade\" value= ".$valida_erro['quantidade']['valor']."></td>";
-                            echo "<tr>";
-                        }
-                    
-                        if($valida_erro['quantidade']['ok']){
-                            echo "<tr>";
-                                echo "<td id=\"table-name\">Quantidade</td>";
-                                echo "<td><input class=\"card-normal\" type=\"number\" name=\"preco\" value= ".$valida_erro['preco']['valor']."></td>";
-                        }                       
-
-                    ?> 
-
-
-                </table>              
-
-                <p><button id="button" type="submit" name="acao" value="Incluir"><h1>Incluir</h1></button></p>
-
-            </form>          
         </section>
 
         <section id="table">
@@ -86,40 +55,19 @@ require_once("conexao.php");
                 <thead>
                     <tr>
                         <th>Nome do Produto</th>
-                        <th>Marca</th>
-                        <th>Quantidadde</th>
-                        <th>Preço</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
-                        global $produtos;
-                        if (count($produtos)){
-                            foreach ($produtos as $produto){
-                                echo "<tr>";
-                                    echo "  <td>" . $produto['nome'] . "</td>";
-                                    echo "  <td>" . $produto['marca'] . "</td>";
-                                    echo "  <td>" . $produto['quantidade'] . "</td>";
-                                    echo "  <td>" . $produto['preco'] . "</td>";
-                                echo "</tr>";
-                            }
-                        }
-                        else{
-                            echo "<table id= \"data-table\">";                            
-                                echo "<tr>";
-                                    echo "  <td><h4>Nehum produto cadastrado</h4></td>";
-                                echo "<tr>";
-                            echo "</table>";
-                        }   
-                    ?>
+                    <!-- imprimir a lista de produtos aqui -->
                 </tbody>
             </table>
+
         </section>
 
     </main>
 
     <footer>
-        <p>Atividade Avaliativa - 2 - Felipe Terra Nascimento de Oliveira</p>
+        <p>Atividade Avaliativa - 5 - Felipe Terra Nascimento de Oliveira</p>
         <p>RGA: 201611640044</p>
     </footer>
 
