09391d6e90cfa19078bdf8cf061d1dba7d23ace7
FelipeTerra <38055797+FelipeTerra@users.noreply.github.com>
2021-07-16 11:52:06 -0400

    Atualização do trabalho
    Fiz algumas alterações seguindo as aulas do professor.

---
+++ AVAL-5-FELIPE-TERRA/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config {
+    public const db_database   = "cadastros";
+    public const db_user       = "root";
+    public const db_password   = "";
+    public const db_host       = "127.0.0.1";
+}
+
+?>
